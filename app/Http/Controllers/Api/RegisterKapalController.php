81fef10d0501858fba82ec4888a509e302e9377d
Sukanda Wiguna <sukandawiguna@mail.unpas.ac.id>
2023-03-31 21:57:52 +0700

    Initial commit

---
+++ app/Http/Controllers/Api/RegisterKapalController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterKapalController extends Controller
+{
+    public function register_kapal(Request $request){
+
+    } 
+
+    public function edit($id){
+
+    } 
+
+    public function hapus($id){
+
+    } 
+
+    public function verifikasi($id){
+        
+    } 
+}
